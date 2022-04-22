8cc89b03d600dda8cba939a33306744ca4694b30
VkAravinth <aravinthmech3392@gmail.com>
2022-04-22 12:58:46 +0530

    first update

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,33 @@
+package org.bank;
+
+import java.io.File;
+
+public  class AxisBank   {
+
+	public static void main(String[] args) {
+		
+		File f = new File("D:\\new"); 
+		
+		boolean a = f.mkdir();
+
+		
+		System.out.println(a);
+		
+		
+		
+		
+	}
+	
+	
+	
+
+ 
+
+
+
+
+
+
+
+
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,23 @@
+package org.bank;
+
+public abstract class BankInfo {
+
+		public void savings() {
+			System.out.println("savings 5%");
+		
+		}
+		
+		public void fixed() {
+			System.out.println("fixed 7%");
+		}
+	
+		public void deposit() {
+			System.out.println("deposit 6%");
+		}
+	
+	
+	
+
+	
+	
+}
